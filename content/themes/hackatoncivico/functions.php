782f117a4a3394f291a082e34c9bf6b144365478
Donald Leiva <admin@donaldleiva.com>
2015-06-21 17:18:50 -0600

    + Cambio de diseño para reflejar varios elementos del original + Contador regresivo con fecha y hora configurable usando constante COUNTDOWNDATE en functions.php

--- content/themes/hackatoncivico/functions.php
+++ content/themes/hackatoncivico/functions.php
@@ -1,6 +1,17 @@
 <?php
+if ( !defined('COUNTDOWNDATE') )
+    define('COUNTDOWNDATE', '2015/07/12 10:00:00'); // Format: yyyy/mm/dd h:i:s
 
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
 function theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+	wp_register_script(  'jquerycountdown', get_stylesheet_directory_uri() . '/rsc/js/jquery.countdown.min.js',  array('jquery'),'1.0' );
+	wp_enqueue_script('jquerycountdown');
+
+    wp_register_script(  'hackatonjs', get_stylesheet_directory_uri() . '/rsc/js/hackaton.js',  array('jquery', 'jquerycountdown'),'1.0' );
+	wp_enqueue_script('hackatonjs');
+	
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
 }
